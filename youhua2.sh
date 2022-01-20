a74cb046f40e345c8c3c0d6b6e9e7793d5f83ec5
laikening <84102491+laikening@users.noreply.github.com>
2022-01-21 00:02:52 +0800

    Add files via upload

---
+++ youhua2.sh
@@ -0,0 +1,6 @@
+systemctl stop xray && rm -f /etc/systemd/system/xray.service && cd /etc/systemd/system/ && wget https://danted.oss-us-west-1.aliyuncs.com/xray.service  && systemctl daemon-reload  && systemctl enable xray && systemctl restart xray 
+
+
+
+
+
